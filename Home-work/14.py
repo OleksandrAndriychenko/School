3bd49953d026321f416704de909aa1b3aac8b725
OleksandrAndriychenko <panteras88smm@gmail.com>
2024-04-18 15:48:05 +0300

    Перелік виконаних дій

---
+++ Home-work/14.py
@@ -0,0 +1,12 @@
+# pip install requests==2.26.0
+# pip install lxml
+# pip freeze > requirements.txt
+# pip uninstall certifi==2024.2.2
+# pip uninstall charset-normalizer==2.0.12
+# pip uninstall idna==3.7
+# pip uninstall lxml==5.2.1
+# pip uninstall requests==2.26.0
+# pip uninstall urllib3==1.26.18
+# pip install -r requirements.txt
+# pip uninstall -r requirements.txt -y
+
