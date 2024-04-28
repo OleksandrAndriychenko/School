c20348380c38472cc9b4d3114584e4f827322b1a
OleksandrAndriychenko <panteras88smm@gmail.com>
2024-04-28 22:05:43 +0300

    Домашня робота 21

---
+++ Home-work/21.py
@@ -0,0 +1,12 @@
+class Auto:
+    brand = None
+    age = 0
+    cоlor = None
+    mark = None
+    weight = 0
+    def move(self):
+        print('move')
+    def birthday(self):
+        age += 1
+    def stop(self):
+        print('stop')
