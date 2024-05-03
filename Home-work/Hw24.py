c187ca3e23b0780544a8164250f7075fc7e4de94
OleksandrAndriychenko <panteras88smm@gmail.com>
2024-05-03 21:22:48 +0300

    Домашня робота 24

--- Home-work/Hw24.py
+++ Home-work/Hw24.py
@@ -1,8 +1,8 @@
 class String(str):
-    def __mul__(self, other):
-        return self + str(other)
+    def __add__(self, other):
+        return str(self) + str(other)
     def __sub__(self, other):
         return self.replace(other, '', 1)
 st_1 = String('pineapple apple pine')
 st_2 = String('apple')
-print(st_1 - st_2)
+print(st_1 + st_2)
